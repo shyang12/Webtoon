0c1ae094a96ed40bf9ca1883ffe57c43d5408e3b
shyang12 <85710913+shyang12@users.noreply.github.com>
2025-01-05 23:38:14 +0900

    25.01.05 WebView Tab으로 구성하기

--- app/src/main/java/com/example/part2chapter1/MainActivity.kt
+++ app/src/main/java/com/example/part2chapter1/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.part2chapter1
 
 import android.os.Bundle
+import android.view.Gravity
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.part2chapter1.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import java.time.temporal.WeekFields
 
 class MainActivity : AppCompatActivity() {
@@ -19,23 +22,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button1.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, WebViewFragment())
-                commit()
-            }
-        }
+        binding.viewPager.adapter = ViewPagerAdapter(this)
 
-        binding.button2.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, BFragment())
-                commit()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            run {
+                val textView = TextView(this@MainActivity)
+                textView.text = "position $position"
+                textView.gravity = Gravity.CENTER
+
+                tab.customView = textView
+                // tab.text = "position $position"
             }
-        }
+        }.attach()
     }
 
     override fun onBackPressed() {
-        val currentFragment = supportFragmentManager.fragments[0]
+        val currentFragment = supportFragmentManager.fragments[binding.viewPager.currentItem]
         if(currentFragment is WebViewFragment) {
             if(currentFragment.canGoBack()) {
                 currentFragment.goBack()
---
+++ app/src/main/java/com/example/part2chapter1/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.part2chapter1
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(private val mainActivity: MainActivity): FragmentStateAdapter(mainActivity) {
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> {
+                return WebViewFragment()
+            }
+            1 -> {
+                return BFragment()
+            }
+            else -> {
+                return WebViewFragment()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+
+}
